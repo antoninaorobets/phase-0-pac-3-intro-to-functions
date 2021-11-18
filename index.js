629e2d776495d88a7d7f03d37d58bc48de8bcd01
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-18 15:42:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!")
+}
+
+sayHello()
+
+function doSomething(thing) {
+    console.log(thing)
+}
+
+doSomething("anything")
+
+
+function add(x, y) {
+    return x + y;
+  }
+  
+console.log(add(1, 2));
+
+let a = add(1, 2);
+console.log(a);
+
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+
+  console.log(say("Hello", "Sofia"));
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
+  
